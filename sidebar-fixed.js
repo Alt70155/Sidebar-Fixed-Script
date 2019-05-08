811e4ac7801490b380cacb7609086b20c8e1be33
chika <chika1086@gmail.com>
2019-05-08 14:12:53 +0900

    add windowSizeJudge

--- sidebar-fixed.js
+++ sidebar-fixed.js
@@ -1,46 +1,61 @@
 const _window      = window
-const _leftBar     = document.querySelector('.left-bar')
-// 上部の隙間(9px)とborder(1px)の10pxを修正
+const _rightBar    = document.querySelector('.right-bar')
+// ヘッダー上部の隙間(9px)とborder(1px)の10pxを足して修正
 const headerHeight = document.querySelector('header').clientHeight + 10
 const _main        = document.querySelector('main')
-// フッター要素までのtopからの絶対座標を取得
-// スクロールされたままリロードされた場合ズレるのでスクロール量も足しておく
+// ブラウザの一番上からfooterまでの位置を取得
+// スクロールされたままリロードされた場合ズレるのでスクロール量(_window.pageYOffset)も足す
+// 更にサイドバーのheight値を足す事により、footer上部までサイドバーの下部が
+// スクロールされた場合の判定ができる
 const footerTop    = document.querySelector('footer')
-  .getBoundingClientRect()
-  .top + _window.pageYOffset - _leftBar.clientHeight
+  .getBoundingClientRect().top
+  + _window.pageYOffset - _rightBar.clientHeight
+// CSSのメティアクエリのmax-width値を設定
+const MAX_WIDTH    = 997
+// メインコンテンツより下(フッター内)の場合のtopの値を計算
+// right-barの位置をスクロール量を合わせて固定表示にする
+const SIDEBAR_TOP_FIXED_VAL = `${_main.getBoundingClientRect().bottom
+  + _window.pageYOffset - _rightBar.clientHeight}px`
+
 
 // サイドバーのposition:left値を更新する関数
 // 画面サイズが変わるたびにleft値を変更する
 const updateLeftValOfSidebar = () => {
-  // mainの右隣にあるため、ブラウザの左端からmainの右までの距離を調べる
-  const mainRight = _main.getBoundingClientRect().right
-  _leftBar.style.left = `${mainRight}px`
+  const rangeUpToMainRight = _main.getBoundingClientRect().right
+  // サイドバーはmainの右隣にあるため、ブラウザの左端からmainの右までの距離を調べる
+  _rightBar.style.left = `${rangeUpToMainRight}px`
 }
-
-// 最初に実行し初期値をセット、以降は画面サイズが変わるたびにleft値を変更
-updateLeftValOfSidebar()
-_window.addEventListener('resize', updateLeftValOfSidebar)
-
-// メインコンテンツより下(フッター内)の場合のtopの値を計算
-// left-barの位置をスクロール量を合わせて固定表示にする
-const SIDEBAR_TOP_FIXED_VAL = `${_main.getBoundingClientRect().bottom +
-                _window.pageYOffset - _leftBar.clientHeight}px`
 
 const fixSidebarWhenScrolled = () => {
   // スクロール量を取得
   const _windowScrollWeight = _window.pageYOffset
   // メインコンテンツ内の場合
   if (_windowScrollWeight > headerHeight && _windowScrollWeight < footerTop) {
-    _leftBar.style.top = `0px`
-    _leftBar.classList.add('fixed')
+    _rightBar.style.top = `0px`
+    _rightBar.classList.add('fixed')
   } else {
-    _leftBar.style.top = `${headerHeight + 1}px`
-    _leftBar.classList.remove('fixed')
-    // メインコンテンツより下(フッター内)の場合
+    _rightBar.style.top = `${headerHeight + 1}px` // borderの1pxを追加
+    _rightBar.classList.remove('fixed')
+    // メインコンテンツより下(フッター内)の場合、固定を解除してその場の高さを設定
     if (_windowScrollWeight > footerTop) {
-      _leftBar.style.top  = SIDEBAR_TOP_FIXED_VAL
+      console.log(SIDEBAR_TOP_FIXED_VAL)
+      _rightBar.style.top  = SIDEBAR_TOP_FIXED_VAL
     }
   }
 }
 
-_window.addEventListener('scroll', fixSidebarWhenScrolled)
+const windowSizeJudge = () => {
+  if (_window.outerWidth > MAX_WIDTH) {
+    fixSidebarWhenScrolled()
+    _window.addEventListener('scroll', fixSidebarWhenScrolled)
+    // 最初に実行し初期値をセット、以降は画面サイズが変わるたびにleft値を変更
+    updateLeftValOfSidebar()
+    _window.addEventListener('resize', updateLeftValOfSidebar)
+  } else {
+    _rightBar.classList.remove('fixed')
+    _window.removeEventListener('scroll', fixSidebarWhenScrolled)
+  }
+}
+
+windowSizeJudge()
+_window.addEventListener('resize', () => windowSizeJudge)
---
+++ sidebar-fixed2.js
@@ -0,0 +1,5 @@
+const _window = window
+const _rightBar = document.querySelector('.right-bar')
+const _main = document.querySelector('main')
+const headerHeight = document.querySelector('header').clientHeight+10
+const footerTop = 
